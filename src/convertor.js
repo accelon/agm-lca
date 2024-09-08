b544843f8a05644dac689c2c0d25f16381b30faa
yapcheahshen <yapcheahshen@gmail.com>
2024-09-08 11:02:41 +0800

    add ^y

--- src/convertor.js
+++ src/convertor.js
@@ -58,7 +58,7 @@ const addN=(content,fn)=>{
 const epilog=(content,fn)=>{
     if (fn=='agmd') {
         let pin=0;
-        content='{id:"agmd-lca",title:"長阿含經"}\n'+content.replace(/佛說長阿含經卷第([一二三四五六七八九○十]+)\n/g,(m,m1)=>'^juan'+fromChineseNumber(m1))
+        content='{id:"agmd-lca",title:"長阿含"}\n'+content.replace(/佛說長阿含經卷第([一二三四五六七八九○十]+)\n/g,(m,m1)=>'^juan'+fromChineseNumber(m1))
         .replace(/_{3,}/g,'')
         .replace(/\^ck(\d+)第([一二三四五六七八九十○]+)經 +([^經]+經)/g,(m,ck,cnum,title)=>title+'\t^ck#d'+ck+'〔'+title+'〕')
         .replace(/\n([^品]{2,4}品)第([一二三四五六七八九十○]+)/g,(m,m1,m2)=>{
@@ -67,7 +67,7 @@ const epilog=(content,fn)=>{
         .replace(/\n閻浮提洲品\t\^ck#d30a〔閻浮提洲品〕/,'\n〔閻浮提洲品〕')
     } else if (fn=='agmm') {
         const refs={}
-        content='{id:"agmm-lca",title:"中阿含經"}\n'+content.replace(/\^ck(\d+)\n（[一二三四五六七八九十○]+）([^【〔\m]*)【([^】\n]+)】/g,(m,ck,title,ref)=>{
+        content='{id:"agmm-lca",title:"中阿含"}\n'+content.replace(/\^ck(\d+)\n（[一二三四五六七八九十○]+）([^【〔\m]*)【([^】\n]+)】/g,(m,ck,title,ref)=>{
             refs[ck]=ref;
             //console.log(title.length)
             return '^ck#m'+ck+'〔'+title+'〕';
@@ -79,10 +79,11 @@ const epilog=(content,fn)=>{
         })
     } else if (fn=='agms') {
         content=process_agms(content);
-        content='{id:"agms-lca",title:"雜阿含經"}\n'+content
+        content='{id:"agms-lca",title:"雜阿含"}\n'+content
     } else if (fn=='agmu') {
-        content='{id:"agmu-lca",title:"增一阿含經"}\n'+content.replace(/增壹阿含經卷第([一二三四五六七八九○十]+)/g,(m,m1)=>'^juan'+fromChineseNumber(m1))
-        .replace(/\^ck(\d+)（([一二三四五六七八九十○]+)）/g,"$2\t^ck$1（$2）")
+        content='{id:"agmu-lca",title:"增一阿含"}\n'
+        +content.replace(/增壹阿含經卷第([一二三四五六七八九○十]+)/g,(m,m1)=>'^juan'+fromChineseNumber(m1))
+        .replace(/\^ck(\d+)（([一二三四五六七八九十○]+)）/g,"$2\t^ck#u$1（$2）")
     }
     return content;;
 }
@@ -90,7 +91,18 @@ export const tagit=(content,fn)=>{
     content=content.replace(/\n?[（\(] ?(\d+[a-z]*)[）\)]/g,(m,m1)=>{
         return '\n^m'+m1.replace(/^0+/g,'');
     })
-    return epilog(content,fn)
+    content= epilog(content,fn)
+    const lines=content.split("\n");
+    let ck='';
+    for (let i=0;i<lines.length;i++) {
+        const m=lines[i].match(/\^ck#([dmsu]\d+)/);
+        const m2=lines[i].match(/\^m(\d+[a-z]*)/);
+        if (m) ck=m[1];
+        else if (m2) {
+            lines[i]=lines[i].replace(/\^m(\d+[a-z]*) */,'^y'+ck+'_'+m2[1]+' ');
+        }
+    }
+    return lines.join('\n')
 }
 export const tidy=content=>{
     content=content
@@ -98,7 +110,7 @@ export const tidy=content=>{
     .replace(/_{3,}/g,'')        
     .replace(/─{3,}/g,'')        
     .replace(/ +\n/g,'\n').replace(/\n +/g,'\n').replace(/\n+/g,'\n')
-    .replace(/\^m(\d+[a-z]?) +/g,'^m$1 ')
+    .replace(/\^y(\d+[a-z]?) +/g,'^y$1 ')
     return content;
 }
 export const topaged=(content)=>{
